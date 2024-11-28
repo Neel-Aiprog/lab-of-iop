b5cc0286543c196b97ad8132e4d29010d54959a7
Neel-Aiprog <mhaskeneel0709@gmail.com>
2024-11-28 12:25:17 +0530

    Update 72.c

--- 72.c
+++ 72.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int n,a[5];
-printf("enter the number of times you want to rotate the array \t");
-scanf("%d",&n);
-for(int i=0;i<5;i++)
-{
-scanf("%d",&a[i]);
-}
-for(int i=n+1;i<5;i++)
-{
-a[i]=a[i-n];
-}
-a[n-1]=a[n-2];
-a[4]=a[n-2];
-for(int i=0;i<5;i++)
-{
-printf("%d",a[i]);
-}
+    int n, a[5];
+    printf("enter the number of times you want to rotate the array \t");
+    scanf("%d", &n);
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = n + 1; i < 5; i++)
+    {
+        a[i] = a[i - n];
+    }
+    a[n - 1] = a[n - 2];
+    a[4] = a[n - 2];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", a[i]);
+    }
 }
